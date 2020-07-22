37b78c00ab925e59c57c9bc0c355a7083744eebc
Marcus Martins <marcusmartins38@gmail.com>
2020-07-22 13:26:28 -0400

    Modal responsive

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import ReactModal from "react-modal";
 
+import "./styles.css";
+
 interface IModalProps {
   isOpen: boolean;
   setIsOpen: () => void;
@@ -21,20 +23,8 @@ const Modal: React.FC<IModalProps> = ({ isOpen, setIsOpen, image }) => {
       onRequestClose={setIsOpen}
       isOpen={modalStatus}
       ariaHideApp={false}
+      className="modal"
       style={{
-        content: {
-          top: "50%",
-          left: "50%",
-          right: "auto",
-          bottom: "auto",
-          marginRight: "-50%",
-          transform: "translate(-50%, -50%)",
-          background: "transparent",
-          color: "#000000",
-          borderRadius: "8px",
-          maxWidth: "1000px",
-          border: "none",
-        },
         overlay: {
           backgroundColor: "#121214e6",
         },
